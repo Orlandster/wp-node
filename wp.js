127ca89cbacd3ab6dd84bfdc3e3510ebe0830da5
Orlando Wenzinger <orlando.wenzinger@bluewin.ch>
2018-10-22 19:07:00 +0200

    [Feature]: Add main config file

---
+++ wp.js
@@ -0,0 +1,11 @@
+import customFields from './config/customFields'
+import customPostTypes from './config/customPostTypes'
+import enqueAdminScripts from './config/enqueAdminScripts'
+import plugins from './config/plugins'
+
+export default const wp = {
+  customFields,
+  customPostTypes,
+  enqueAdminScripts,
+  plugins,
+}
